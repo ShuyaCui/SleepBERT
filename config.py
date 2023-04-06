928087023be0336c381b4806b730d6e467ebdcf5
ShuyaCui <126207792+ShuyaCui@users.noreply.github.com>
2023-04-06 00:33:05 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-RAW_DATASET_ROOT_FOLDER = '/lustre/home/acct-clslh/clslh/csy/sleep/Data'
+RAW_DATASET_ROOT_FOLDER = '/data1/tmp/wangzhaobin/UKB/csy_ukb/sleep/Data'
 
 STATE_DICT_KEY = 'model_state_dict'
 OPTIMIZER_STATE_DICT_KEY = 'optimizer_state_dict'
