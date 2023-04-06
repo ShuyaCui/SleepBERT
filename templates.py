d01a5f542d52fe5d5d18c953573f4514fd6815a7
ShuyaCui <126207792+ShuyaCui@users.noreply.github.com>
2023-04-05 23:40:34 -0400

    Update templates.py

--- templates.py
+++ templates.py
@@ -6,7 +6,7 @@ def set_template(args):
         args.mode = 'train'
 
         args.dataloader_code = 'bert'
-        batch = 1024
+        batch = 512
         if args.dataset_code=='UKB':
             args.bert_dropout = 0.3
             args.tau = 0.3
